fe6fb6c92083a6a344ce89f434e2c4f4fc62e57e
gyubin-code <sdhg12@naver.com>
2021-05-29 00:17:18 +0900

    Apply meterial

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,39 +1,24 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{
     render(){
         //render는 항상 수행되는 화면
         return(
-        <div>
-          <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-          <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-        </div>
+        <TableRow>
+            <TableCell>{this.props.id} </TableCell>
+            <TableCell><img src={this.props.image} alt='profile'/> </TableCell>
+            <TableCell>{this.props.name} </TableCell>
+            <TableCell>{this.props.birthday} </TableCell>
+            <TableCell>{this.props.gender} </TableCell>
+            <TableCell>{this.props.job} </TableCell>
+        </TableRow>
         )
     }
 }
+ 
 
-class CustomerProfile extends React.Component{
-    // id 값 img값 렌더링하는것
-    render(){
-        return(
-            <div>
-                <img scr={this.props.image} alt="profile"/>
-                <h2> {this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-class CustomerInfo extends React.Component{
-    render(){
-        //r//사용자의 남은 정보
-        return(
-        <div>
-            <p>{this.props.birthday}</p>
-            <p>{this.props.gender}</p>
-            <p>{this.props.job}</p>
-        </div>
-        )
-    }
-}
+
 
 export default Customer;//custermer class 를 내보내는 역할
